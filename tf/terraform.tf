6b707badb4305c76dfe6dd5fd70b8e3ee67c0808
Feng Ruohang <rh@vonng.com>
2026-02-12 17:12:11 +0800

    use NoSpot policy for build machine

--- tf/terraform.tf
+++ tf/terraform.tf
@@ -15,7 +15,7 @@
 locals {
   bandwidth = 100                       # internet bandwidth in Mbps (100Mbps)
   disk_size = 100                       # system disk size in GB (100GB)
-  spot_policy = "SpotAsPriceGo"         # NoSpot, SpotWithPriceLimit, SpotAsPriceGo
+  spot_policy = "NoSpot"                # NoSpot, SpotWithPriceLimit, SpotAsPriceGo
   spot_price_limit = 5                  # only valid when spot_policy is SpotWithPriceLimit
   instance_type_map = {
     amd64 = "ecs.c9i.4xlarge"
